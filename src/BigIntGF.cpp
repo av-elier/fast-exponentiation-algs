e78a0d47febabff4f8bc6db8b81fbad813a436ce
Adelier <adelier@hotmail.com>
2013-10-26 17:25:56 +0400

    Branch test

---
+++ src/BigIntGF.cpp
@@ -0,0 +1,25 @@
+/*
+ * BigIngGF.cpp
+ *
+ *  Created on: 26 окт. 2013 г.
+ *      Author: Adelier
+ */
+
+class BigIntGF {
+public:
+	BigIntGF();
+	virtual ~BigIntGF();
+
+
+
+	BigIntGF mult(BigIntGF y){
+		BigIntGF result = BigIntGF();
+
+		return result;
+	}
+	BigIntGF sqr(){
+		BigIntGF result = BigIntGF();
+
+		return result;
+	}
+};
--- src/fast-exponention-algs-main.cpp
+++ src/fast-exponention-algs-main.cpp
@@ -1,15 +1,21 @@
 //============================================================================
-// Name        : fast-exponention-algs.cpp
+// Name        : fast-exponention-algs-main.cpp
 // Author      : Adelier
-// Version     :
+// Version     : 0.0.0
 // Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Description : Homework 1-st year graduate students mmcs.sfedu.ru
 //============================================================================
 
 #include <iostream>
+
+#include "BigIntGF.h"
+
 using namespace std;
 
 int main() {
+	BigIntGF big = BigIntGF();
+	BigIntGF big2 = big.sqr();
+
 	cout << "Hello World" << endl; // prints Hello World
 	return 0;
 }
