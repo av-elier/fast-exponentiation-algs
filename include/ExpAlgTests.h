fa0ef07330817b071564c9ae949dadd15bebe256
Adelier <adelier@hotmail.com>
2013-11-01 23:23:05 +0400

    Ready for public

---
+++ include/ExpAlgTests.h
@@ -0,0 +1,61 @@
+/*
+ * MyTests.h
+ *
+ *  Created on: 28 окт. 2013 г.
+ *      Author: Adelier
+ */
+
+#ifndef MYTESTS_H_
+#define MYTESTS_H_
+
+#include <vector>
+
+#include "NTL/ZZ.h"
+#include "NTL/ZZ_p.h"
+
+#include "ExpAlgInterfaces.h"
+#include "algorithms/adelier/RightToLeftByAdelier.h"
+
+NTL_CLIENT;
+
+namespace MyTests {
+
+static bool testExpAlg(ExpAlg *algo){
+
+	ZZ prime = conv<ZZ>(59);
+	ZZ_p::init(prime);
+
+	ZZ_p samplex = conv<ZZ_p>(conv<ZZ>(2));
+	ZZ samplen = conv<ZZ>(4);
+	if ( algo->exp(samplex, samplen) != 16 ) // assert true
+		return false;
+
+	samplex = conv<ZZ_p>(conv<ZZ>("11"));
+	samplen = conv<ZZ>("6");
+	if ( algo->exp(samplex, samplen) != 27 ) // assert true
+		return false;
+
+	return true;
+}
+static bool test(ExpAlg *algo){
+	bool success = true; // optimistic, yes
+	if (testExpAlg(algo))
+		cout<<"test success"<<endl;
+	else {
+		cout<<"TEST FAILED!"<<endl;
+		success = false;
+	}
+	return success; // lie
+}
+static bool testAll(vector<ExpAlg*> algos){
+	bool success = true;
+	for (unsigned int i = 0; i < algos.size(); ++i){
+		success = success && test(algos[i]);
+	}
+	return success;
+}
+
+
+} /* namespace RightToLeftByAdelier */
+
+#endif /* MYTESTS_H_ */
---
+++ include/algorithms/adelier/RightToLeftByAdelier.h
@@ -0,0 +1,34 @@
+/*
+ * RightToLeftByAdelier.h
+ *
+ *  Created on: 28 ���. 2013 �.
+ *      Author: Adelier
+ */
+
+#ifndef RIGHTTOLEFTBYADELIER_H_
+#define RIGHTTOLEFTBYADELIER_H_
+/*
+ * RightToLeftByAdelier.h
+ *
+ *  Created on: 28 ���. 2013 �.
+ *      Author: Adelier
+ */
+
+#include "ExpAlgInterfaces.h"
+#include "NTL\ZZ.h"
+#include "NTL\ZZ_p.h"
+using namespace NTL;
+
+
+namespace Adelier {
+
+class RightToLeft : public ExpAlg {
+public:
+	RightToLeft();
+	virtual ~RightToLeft();
+	ZZ_p exp(ZZ_p x, ZZ n);
+};
+
+} /* namespace Adelier */
+
+#endif /* RIGHTTOLEFTBYADELIER_H_ */
